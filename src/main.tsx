89bdae8666fae8164a2d0c20a1126d0c27d4f9d8
zaftyslogistics-code <zaftyslogistics@gmail.com>
2026-04-29 19:51:26 +0530

    fix: add QueryClientProvider to fix null stores error

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./styles.css";
 
@@ -14,10 +15,14 @@ metaTag.name = "build-time";
 metaTag.content = deploymentTimestamp;
 document.head.appendChild(metaTag);
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
